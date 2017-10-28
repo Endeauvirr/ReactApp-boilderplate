c9fa6641388e9d112d34bb45d97f931a7881b8e6
Michał Gołębiowski <michal.golebiowski@unity.pl>
2017-10-28 12:34:53 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        hot: true,
+        inline: true,
+        port: 8080
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
